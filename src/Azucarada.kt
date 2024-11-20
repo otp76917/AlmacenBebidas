49215bbbfec7afb4d8389d4d4c51309fa1df84db
Javier Rodríguez Martínez <undinepapirus@gmail.com>
2024-11-20 19:40:22 +0100

    azucarada works

--- src/Azucarada.kt
+++ src/Azucarada.kt
@@ -3,6 +3,6 @@ class Azucarada (id:Int,litros:Float,precio:Float,marca:String,val porcentaje:Fl
         return "id=$id litros=$litros precio=$precio marca=$marca porcentaje=$porcentaje promocion=$promocion"
     }
     init {
-        if(this.promocion) precio *= 0.9f
+        if(this.promocion) this.precio *= 0.9f
     }
 }
