2da1a398a8a9b8d1a528ac3f52958bb2e8acd712
Javier Rodríguez Martínez <undinepapirus@gmail.com>
2024-11-20 18:26:08 +0100

    precio to var

--- src/Bebida.kt
+++ src/Bebida.kt
@@ -1,4 +1,4 @@
-open class Bebida (val id:Int,val litros:Float,val precio:Float,val marca:String) {
+open class Bebida (val id:Int,val litros:Float,var precio:Float,val marca:String) {
     override fun toString(): String {
         return "id=$id litros=$litros precio=$precio marca=$marca"
     }
